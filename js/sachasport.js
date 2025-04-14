b88f1e396b225fcce2b2e81a4240c8cfc5fcd8ed
Tomáš Gánoci <tomas.ganoci@gmail.com>
2025-04-14 10:43:36 +0200

    Vypnutí externích doplňků

--- js/sachasport.js
+++ js/sachasport.js
@@ -1,10 +1,10 @@
 import './doplnky/_automaticke_nacitani_dalsich_produktu-modifikovany.js';
-import './doplnky/_hodnoceni.js';
+// import './doplnky/_hodnoceni.js';
 import './doplnky/_infohlaska_v_detailu.js';
-import './doplnky/_sbaleni_filtru_v_kategorii.js';
+// import './doplnky/_sbaleni_filtru_v_kategorii.js';
 import './doplnky/_sipky-v-obrazcich.js';
 import './doplnky/_tabulka-velikosti.js';
-import './doplnky/_zvolene-filtry.js';
+// import './doplnky/_zvolene-filtry.js';
 import './doplnky/_back_to_top_sipka.js'
 
 import './sablona_classic/_presun_popisu_kategorie_nad_subkategorie.js';
